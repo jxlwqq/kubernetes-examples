5fc114be15e3f36844d02dbca5de410faca6e19f
jxlwqq <jxlwqq@gmail.com>
2021-06-25 11:58:37 +0800

    wip

---
+++ deploying-simple-web-app-with-gin-step-by-step/go.mod
@@ -0,0 +1,5 @@
+module hello-gin
+
+go 1.16
+
+require github.com/gin-gonic/gin v1.7.2 // indirect
---
+++ deploying-simple-web-app-with-gin-step-by-step/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(200, "Hello, Gin!")
+	})
+
+	_ = r.Run(":8080")
+}
